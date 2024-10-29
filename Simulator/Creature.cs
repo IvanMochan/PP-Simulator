5e6447db4bca5834506be3cf0977c7f4c2051c7f
IvanMochan <146974396+IvanMochan@users.noreply.github.com>
2024-10-29 19:55:13 +0100

    Stwory

---
+++ Simulator/Creature.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator;
+public class Creature
+{
+    string Name { get; set; }
+    int Level { get; set; }
+    public string Info => $"{Name} [{Level}]";
+    public Creature()
+    {
+
+    }
+    public Creature(string name, int level = 1)
+    {
+        Name = name;
+        Level = level;
+    }
+    public void SayHi() => Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+
+}
