88313b694f41e1efa6fb92af80c1b446ca521da3
IvanMochan <146974396+IvanMochan@users.noreply.github.com>
2024-10-29 20:01:33 +0100

    Settery

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -4,9 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Simulator
+namespace Simulator;
+public class Animals
 {
-    internal class Animals
+    private string _description = "Unknown";
+    public required string Description
     {
+        get { return _description; }
+        init
+        {
+            _description = value.Trim();
+            if (_description.Length > 15)
+            {
+                _description = _description.Remove(15);
+                _description = _description.Trim();
+            }
+            if (_description.Length < 3)
+                _description = _description.PadRight(3, '#');
+            if (char.IsLower(_description[0]))
+            {
+                _description = char.ToUpper(_description[0]) + _description.Substring(1);
+            }
+        }
     }
+    public uint Size { get; set; } = 3;
+
+    public string Info => $"{Description} <{Size}>";
 }
