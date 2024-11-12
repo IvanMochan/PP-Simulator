16201d502360666da6062046a092fde266a4a0bf
Ivan <235393@student.uek.krakow.pl>
2024-11-12 22:58:05 +0100

    Prostokąty

---
+++ Simulator/Point.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator;
+
+public readonly struct Point
+{
+    public readonly int X, Y;
+    public Point(int x, int y) => (X, Y) = (x, y);
+    public override string ToString() => $"({X}, {Y})";
+
+    public Point Next(Direction direction)
+    {
+        int new_x = X;
+        int new_y = Y;
+
+        switch (direction)
+        {
+            case Direction.Left:
+                new_x--; break;
+            case Direction.Right:
+                new_x++; break;
+            case Direction.Up:
+                new_y++; break;
+            case Direction.Down:
+                new_y--; break;
+        }
+        return new Point(new_x, new_y);
+    }
+
+
+    public Point NextDiagonal(Direction direction)
+    {
+        int new_x = X;
+        int new_y = Y;
+
+        switch (direction)
+        {
+            case Direction.Left:
+                new_x--; new_y++; break;
+            case Direction.Right:
+                new_x++; new_y--; break;
+            case Direction.Up:
+                new_x++; new_y++; break;
+            case Direction.Down:
+                new_x--; new_y--; break;
+        }
+        return new Point(new_x, new_y);
+    }
+}
+    
--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -2,60 +2,54 @@
 
 internal class Program
 {
-    static void Lab4a()
+    static void Lab5a()
     {
-        Console.WriteLine("HUNT TEST\n");
-        var o = new Orc() { Name = "Gorbag", Rage = 7 };
-        o.SayHi();
-        for (int i = 0; i < 10; i++)
-        {
-            o.Hunt();
-            o.SayHi();
+        try 
+        { 
+            var rec1 = new Rectangle(4, 2, 2, 7); // Wynik: (2,2):(4,7)
+            Console.WriteLine($"Prostokont 1 : {rec1}");
+
+            var point1 = new Point(9, 5);
+            var point2 = new Point(4, 6);
+            var rec2 = new Rectangle(point1, point2); // Wynik: (5,4):(9,6)
+            Console.WriteLine($"Prostokont 2 : {rec2}");
+
+            var point3 = new Point(5, 5);
+            var rec3 = new Rectangle(point1, point3);
+            Console.WriteLine($"Prostokont 3 : {rec3}"); // Błędne dane
         }
 
-        Console.WriteLine("\nSING TEST\n");
-        var e = new Elf("Legolas", agility: 2);
-        e.SayHi();
-        for (int i = 0; i < 10; i++)
+        catch (ArgumentException ex)
         {
-            e.Sing();
-            e.SayHi();
+            Console.WriteLine($"Błędne dane: {ex.Message} ");
         }
 
-        Console.WriteLine("\nPOWER TEST\n");
-        Creature[] creatures = {
-        o,
-        e,
-        new Orc("Morgash", 3, 8),
-        new Elf("Elandor", 5, 3)
-    };
-        foreach (Creature creature in creatures)
-        {
-            Console.WriteLine($"{creature.Name,-15}: {creature.Power}");
-        }
+        var rec4 = new Rectangle(1, 1, 6, 7);
+        Point insidep = new Point(4, 4);
+        Point outsidep = new Point(10, 10);
+
+        Console.WriteLine($"Czy prostokąt zawiera punkt (3, 4): {rec4.Contains(insidep)}"); // True
+        Console.WriteLine($"Czy prostokąt zawiera punkt (9, 9): {rec4.Contains(outsidep)}"); // False
+
+
+
+        Point start = new Point(5, 5);
+        Console.WriteLine($"Punkt startowy: {start}");
+
+        Console.WriteLine($"Next (Left): {start.Next(Direction.Left)}");      // Wynik: (4,5)
+        Console.WriteLine($"Next (Right): {start.Next(Direction.Right)}");    // Wynik: (6,5)
+        Console.WriteLine($"Next (Up): {start.Next(Direction.Up)}");          // Wynik: (5,6)
+        Console.WriteLine($"Next (Down): {start.Next(Direction.Down)}");      // Wynik: (5,4)
+
+        Console.WriteLine($"NextDiagonal (Left): {start.NextDiagonal(Direction.Left)}");     // Wynik: (4,6)
+        Console.WriteLine($"NextDiagonal (Right): {start.NextDiagonal(Direction.Right)}");   // Wynik: (6,4)
+        Console.WriteLine($"NextDiagonal (Up): {start.NextDiagonal(Direction.Up)}");         // Wynik: (6,6)
+        Console.WriteLine($"NextDiagonal (Down): {start.NextDiagonal(Direction.Down)}");     // Wynik: (4,4)
     }
-    static void Lab4b()
-    {
-        object[] myObjects = {
-        new Animals() { Description = "dogs"},
-        new Birds { Description = "  eagles ", Size = 10 },
-        new Elf("e", 15, -3),
-        new Orc("morgash", 6, 4)
-    };
-        Console.WriteLine("\nMy objects:");
-        foreach (var o in myObjects) Console.WriteLine(o);
-        /*
-            My objects:
-            ANIMALS: Dogs <3>
-            BIRDS: Eagles (fly+) <10>
-            ELF: E## [10][0]
-            ORC: Morgash [6][4]
-        */
-    }
+
     static void Main(string[] args)
     {
         Console.WriteLine("Starting Simulator!\n");
-        Lab4a();
-        Lab4b();
+        Lab5a();
     }
 }
